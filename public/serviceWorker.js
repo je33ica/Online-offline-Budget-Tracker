8f8f09cda92c37f5eebb077d3fa9d1f3ea0e6f2e
je33ica <je331687@gmail.com>
2021-03-19 13:32:27 +0000

    trtying to fix bug - not working properly when launched on heroku

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -21,27 +21,27 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// // The activate handler takes care of cleaning up old caches.
-// self.addEventListener("activate", (event) => {
-//   const currentCaches = [CACHE, DATACACHE];
-//   event.waitUntil(
-//     caches
-//       .keys()
-//       .then((cacheNames) => {
-//         return cacheNames.filter(
-//           (cacheName) => !currentCaches.includes(cacheName)
-//         );
-//       })
-//       .then((cachesToDelete) => {
-//         return Promise.all(
-//           cachesToDelete.map((cacheToDelete) => {
-//             return caches.delete(cacheToDelete);
-//           })
-//         );
-//       })
-//       .then(() => self.clients.claim())
-//   );
-// });
+// The activate handler takes care of cleaning up old caches.
+self.addEventListener("activate", (event) => {
+  const currentCaches = [CACHE, DATACACHE];
+  event.waitUntil(
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return cacheNames.filter(
+          (cacheName) => !currentCaches.includes(cacheName)
+        );
+      })
+      .then((cachesToDelete) => {
+        return Promise.all(
+          cachesToDelete.map((cacheToDelete) => {
+            return caches.delete(cacheToDelete);
+          })
+        );
+      })
+      .then(() => self.clients.claim())
+  );
+});
 
 self.addEventListener("fetch", (event) => {
   if (event.request.url.includes("/api/")) {
