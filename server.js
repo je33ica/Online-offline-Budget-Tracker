a9ef5397b5632cd3e4bf302a4acbe506c7536192
je33ica <je331687@gmail.com>
2021-03-19 12:49:34 +0000

    env for heroku added

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+//local or enviromental for hosting on Heroku
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
